2344926ab27eed8dc34212fd50f17fffd3ea1b66
Alex Taraba <161524033+Artaraba@users.noreply.github.com>
2024-07-21 21:18:18 +0000

    Added main method and print statement

---
+++ PopulationProjectionProject.java
@@ -0,0 +1,19 @@
+//*| Created by: Alex Taraba                                                                          |*\\
+//*|                                                                                                  |*\\
+//*| Created on: 07/21/2024                                                                           |*\\
+//*|                                                                                                  |*\\
+//*| Intent: This program is meant to take input from users concerning the following questions:       |*\\
+//*|                1) What is the birthrate (births per hour/min/sec)?                               |*\\
+//*|                 2) What is the death rate (deaths per hour/min/sec)?                             |*\\
+//*|                3) What is the immigration rate (immigrations per hour/min/sec)?                  |*\\
+//*|            (Default to births/deaths/immigrations per sec if no units are secified)              |*\\
+//*| and produce a population growth rate projection (numbers only) number on the display screen.     |*\\
+
+// Import necessary java classes
+import java.util.Scanner;
+
+public class PupulationProjectionProject {
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+    }
+}
