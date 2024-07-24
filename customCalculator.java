3fbb21d4ff5556722a14a7c892f61a28c23858b0
Alex Taraba <161524033+Artaraba@users.noreply.github.com>
2024-07-24 20:28:03 +0000

    Added some comments

--- customCalculator.java
+++ customCalculator.java
@@ -1,7 +1,10 @@
+// Import necessary classes
+import javafx.application.Application;
+
 //  Begin customCalculator class
 public class customCalculator {
   // Begin main(String[] args) method
   public static void main(String[] args) {
     System.out.print("Hello World");
   } // End main(String[] args) method
-}
+} // End class customCalculator 
