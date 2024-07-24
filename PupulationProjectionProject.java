56e6c6663a685a4ef9d93083fbf21509c7062388
Alex Taraba <161524033+Artaraba@users.noreply.github.com>
2024-07-24 17:51:25 +0000

    Completed algorthim to computer year populations a display to user

--- PupulationProjectionProject.java
+++ PupulationProjectionProject.java
@@ -22,11 +22,33 @@ public class PupulationProjectionProject {
         System.out.println();
         System.out.println("****************************************************************************************************");
         // Get birthrate from user
-        System.out.print("Please enter a birthrate (births per sec, whole number format only): ");
+        System.out.print("Please enter a birth rate (births per sec, whole number format only): ");
         int birthRate = input.nextInt();
         System.out.println();
-        // Get deathrate from user
-        System.out.print("Please enter a deathrate (deaths per secm whole numbers only) : ");
+        // Get death rate from user
+        System.out.print("Please enter a death rate (deaths per sec whole numbers only): ");
         int deathRate = input.nextInt();
+        System.out.println();
+        // Get immigration rate from user
+        System.out.print("Please enter an immigration rate (immigrations per sec, whole numbers only): ");
+        int immigrationRate = input.nextInt();
+        System.out.println();
+        // Get current population
+        System.out.print("Please enter the current population (whole humbers only): ");
+        int currentPopulation = input.nextInt();
+        System.out.println();
+        // Find population growth rate
+        double growthRate = ((birthRate + immigrationRate) - (deathRate)) / 100.00;
+        // Calculate population projection for each year in the next five years
+        double yearOnePop = currentPopulation * Math.pow((1 + growthRate), 1);
+        double yearTwoPop = currentPopulation * Math.pow((1 + growthRate), 2);
+        double yearThreePop = currentPopulation * Math.pow((1 + growthRate), 3);
+        double yearFourPop = currentPopulation * Math.pow((1 + growthRate), 4);
+        double yearFivePop = currentPopulation * Math.pow((1 + growthRate), 5);
+        System.out.println("Year one pop: " + yearOnePop);
+        System.out.println("Year two pop: " + yearTwoPop);
+        System.out.println("Year three pop: " + yearThreePop);
+        System.out.println("Year four pop: " + yearFourPop);
+        System.out.println("Year five pop: " + yearFivePop);
     }
 }
