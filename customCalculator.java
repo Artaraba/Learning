673e9ae35aa57f8753f4fdaa746a788643fb2761
Alex Taraba <161524033+Artaraba@users.noreply.github.com>
2024-07-24 20:22:36 +0000

    Add print statement to test output

---
+++ customCalculator.java
@@ -0,0 +1,7 @@
+//  Begin customCalculator class
+public class customCalculator {
+  // Begin main(String[] args) method
+  public static void main(String[] args) {
+    System.out.print("Hello World");
+  } // End main(String[] args) method
+}
