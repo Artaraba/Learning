25ba73e6b31c563208294d0b50243e5e044749d9
Alex Taraba <161524033+Artaraba@users.noreply.github.com>
2024-07-26 21:44:26 +0000

    Created initial file

--- PointInRectangle.java
+++ PointInRectangle.java
@@ -1,1 +1,25 @@
+/* A Taraba Tech software
+* 
+* Created by: Alex Taraba
+* Date of origin: 07/26/2024
+* Version: 0
+* This program is meant to get a point from the user then determine if it falls
+* inside the rectangle which is centered at 0,0
+*
+*/
 
+// Import necessary classes
+import java.util.Scanner;
+
+// Begin class PointInRectangle
+public class PointInRectangle {
+    // Begin main(String[] args) method
+    public static void main(String[] args) {
+        // Create scanner object to capture user input
+        Scanner input = new Scanner(System.in);
+        // Get user input
+        System.out.print("Please enter a coordinate in the form of (x, y): ");
+        double x = input.nextDouble();
+        double y = input.nextDouble();
+    } // End main(String[] args) method
+} // End class PointInRectangle
